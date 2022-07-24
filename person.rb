0253d458b1fdb3b072a08826be9f65066ac9748c
Joshua O. Ivie <joshua@lifeonline.com.ng>
2022-07-24 05:04:54 +0100

    Add App

--- person.rb
+++ person.rb
@@ -1,15 +1,12 @@
-require 'uuid'
 require_relative 'nameable'
 
 class Person < Nameable
   attr_reader :id, :rentals
   attr_accessor :age, :name
 
-  ID = UUID.new
-
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = ID.generate
+    @id = Random.rand(1..500)
     @name = name
     @age = age
     @parent_permission = parent_permission
